e0b3a9840be1aabda4f42e1230d37c8d156a2a67
CaioProg <caioprog18@gmail.com>
2022-06-17 20:12:51 -0300

    Conversão explícita de tipos numéricos

---
+++ Conversao/Conversao/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Conversão Implícita
+            //    byte num1 = 100; // 8 bits, de 0 a 255
+            //    ushort num2; // 16 bits, 0 a 65.535
+
+            //    float num3 = 1250.45f;
+            //    double num4 = num3;
+
+            //    num3 = num1;
+            //    num2 = num1;
+
+            //    char letra = 'C';
+            //    int numero = 'C';
+
+            //    Console.WriteLine(num2);
+            //    Console.WriteLine(num3);
+            //    Console.WriteLine(numero);
+            #endregion
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
