d3cbec2ce734fba47a75ef8ba242ed65923b9d20
CaioProg <caioprog18@gmail.com>
2022-06-17 19:43:25 -0300

    Métodos de Entrada e Saída de Dados em C#"

---
+++ EntradaSaida/EntradaSaida/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaSaida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Caio");
+            //Console.Write("Lucas");
+
+            //int codigo = Console.Read();
+            //Console.WriteLine(codigo);
+
+            string texto = Console.ReadLine();
+
+            Console.WriteLine(texto);
+
+            Console.ReadKey();  
+        }
+    }
+}
