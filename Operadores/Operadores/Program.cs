859ccaf3acb6d206909cdc29156cf1a475c020a9
CaioProg <caioprog18@gmail.com>
2022-06-17 20:58:20 -0300

    Operadores aritméticos (matemáticos)

---
+++ Operadores/Operadores/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 130;
+            int num2 = 2;
+            int resultado = num1 + num2;
+
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine(num1 - num2);
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine(num1 / num2);
+            Console.WriteLine(num1 % num2);
+
+
+            Console.ReadKey();
+        }
+    }
+}
