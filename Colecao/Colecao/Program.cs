717381d69a803bbf91e720658da36bbc23eee9bd
CaioProg <caioprog18@gmail.com>
2022-06-18 15:14:37 -0300

    Vetor - Array unidimencional

---
+++ Colecao/Colecao/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Vetores
+            //int[] numeros = new int[5];
+
+            //numeros[0] = 10;
+            //numeros[1] = 20;
+            //numeros[2] = 30;
+            //numeros[3] = 40;
+            //numeros[4] = 50;
+
+            //numeros[2] = 55;
+
+            //string[] nomes =
+            //{
+            //    "Caio",
+            //    "Lucas",
+            //    "Maria"
+            //};
+
+            //nomes[0] = "Logan";
+
+
+            //Console.WriteLine(numeros[2]);
+            //Console.WriteLine(nomes[0]);
+            #endregion
+
+
+            
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
