7c5c336c7f99d6de79895eea7d5826cce583bf24
CaioProg <caioprog18@gmail.com>
2022-06-18 10:40:38 -0300

    Precedência de operadores aritméticos (prioridade de sinais)

--- Operadores/Operadores/Program.cs
+++ Operadores/Operadores/Program.cs
@@ -10,15 +10,28 @@ namespace Operadores
     {
         static void Main(string[] args)
         {
-            int num1 = 130;
-            int num2 = 2;
-            int resultado = num1 + num2;
+            #region Operadores matemáticos
+            //int num1 = 130;
+            //int num2 = 2;
+            //int resultado = num1 + num2;
 
-            Console.WriteLine(num1 + num2);
-            Console.WriteLine(num1 - num2);
-            Console.WriteLine(num1 * num2);
-            Console.WriteLine(num1 / num2);
-            Console.WriteLine(num1 % num2);
+            //Console.WriteLine(num1 + num2);
+            //Console.WriteLine(num1 - num2);
+            //Console.WriteLine(num1 * num2);
+            //Console.WriteLine(num1 / num2);
+            //Console.WriteLine(num1 % num2);
+            #endregion
+
+            #region Precedência de Operadores
+            //double num1 = 100;
+            //double num2 = 10;
+            //double num3 = 5;
+
+            //double res = (num1 + num2) * (num3 / 3);
+
+            //Console.WriteLine(res);
+            #endregion
+
 
 
             Console.ReadKey();
