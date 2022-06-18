514dfc2b1e011f6d13278dfc969f57fb39e73e18
CaioProg <caioprog18@gmail.com>
2022-06-18 15:30:38 -0300

    Matriz - Array bidimensional

--- Colecao/Colecao/Program.cs
+++ Colecao/Colecao/Program.cs
@@ -35,9 +35,51 @@ namespace Colecao
             //Console.WriteLine(nomes[0]);
             #endregion
 
+            #region Matriz
+            int[,] numeros = new int[2, 3];
 
-            
+            numeros[0,0] = 10;
+            numeros[0,1] = 20;
+            numeros[0,2] = 30;
+
+            numeros[1,0] = 40;
+            numeros[1,1] = 50;
+            numeros[1,2] = 60;
+
+            Console.WriteLine("Matriz de Inteiros\n");
+
+            Console.Write("[" + numeros[0,0] + "]");
+            Console.Write("[" + numeros[0,1] + "]");
+            Console.Write("[" + numeros[0,2] + "]");
+
             Console.WriteLine();
+
+            Console.Write("[" + numeros[1, 0] + "]");
+            Console.Write("[" + numeros[1, 1] + "]");
+            Console.Write("[" + numeros[1, 2] + "]");
+
+            Console.WriteLine();
+
+            // Outra forma de declarar a matriz
+            string[,] nomes =
+            {
+                { "Caio", "Lucas", "João" },    
+                { "Maria", "Julia", "Bia" }
+            };
+
+            Console.WriteLine("\nMatriz de String\n");
+
+            Console.Write("[" + nomes[0,0] + "]");
+            Console.Write("[" + nomes[0,1] + "]");
+            Console.Write("[" + nomes[0,2] + "]");
+
+            Console.WriteLine();
+
+            Console.Write("[" + nomes[1, 0] + "]");
+            Console.Write("[" + nomes[1, 1] + "]");
+            Console.Write("[" + nomes[1, 2] + "]");
+            #endregion
+
             Console.ReadKey();
         }
     }
