9bf9a89e75f3ea092d71c75fae6098c0437061c6
CaioProg <caioprog18@gmail.com>
2022-06-19 16:41:50 -0300

    Métodos com parâmetros

---
+++ POO/MetodosDasClasses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosDasClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodos m = new Metodos();
+            m.Cumprimentar();
+            m.Somar(12, 45);
+            m.Apresentar("Caio", 19);
+
+            Console.ReadKey();
+        }
+    }
+}
