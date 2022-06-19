cc75091408436e1a14745a338dc11779db828aca
CaioProg <caioprog18@gmail.com>
2022-06-18 22:00:57 -0300

    Estruturas condicionais simples (if-else)

---
+++ Condicao/Condicao/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 5;
+
+            if(valor > 5)
+            {
+                Console.WriteLine("Condição verdadeira");
+            }
+            else
+            {
+                Console.WriteLine("Condição retornou falso");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
