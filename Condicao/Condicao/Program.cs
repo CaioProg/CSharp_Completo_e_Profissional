c27252fb59cb523b5daa4b2cd64f5084ad9e6524
CaioProg <caioprog18@gmail.com>
2022-06-18 22:18:40 -0300

    Estruturas condicionais aninhadas

--- Condicao/Condicao/Program.cs
+++ Condicao/Condicao/Program.cs
@@ -10,24 +10,46 @@ namespace Condicao
     {
         static void Main(string[] args)
         {
-            int valor = 1;
+            #region Condição encadeada
+            //int valor = 1;
 
-            if(valor < 5)
-            {
-                Console.WriteLine("Condição verdadeira");
-            }
-            else if(valor >= 5 && valor < 10)
-            {
-                Console.WriteLine("Condição alternativa 1");
-            }
-            else if(valor >= 10 && valor < 20)
-            {
-                Console.WriteLine("Condição alternativa 2");
-            }
-            else
-            {
-                Console.WriteLine("Condição alternativa final");
-            }
+            //if(valor < 5)
+            //{
+            //    Console.WriteLine("Condição verdadeira");
+            //}
+            //else if(valor >= 5 && valor < 10)
+            //{
+            //    Console.WriteLine("Condição alternativa 1");
+            //}
+            //else if(valor >= 10 && valor < 20)
+            //{
+            //    Console.WriteLine("Condição alternativa 2");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Condição alternativa final");
+            //}
+            #endregion
+
+            #region Condição aninhada
+            //int numero = 1;
+
+            //if (numero > 5)
+            //{
+            //    Console.Write("O número é maior que 5");
+
+            //    if (numero % 2 == 0)
+            //    {
+            //        Console.WriteLine(" e também é par.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(" mas não é par.");
+            //    }
+            //}
+            #endregion
+
+
 
             Console.ReadKey();
         }
