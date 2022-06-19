981c15432c5ebdfd7119c4f00ad4395667b56e21
CaioProg <caioprog18@gmail.com>
2022-06-19 15:02:07 -0300

    Estrutura de repetição (ciclo while)

---
+++ Ciclos/Ciclos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Ciclo While
+            //int valor = 15;
+            //while (valor < 10)
+            //{
+            //    Console.WriteLine($"O valor é: {valor}");
+            //    valor++;
+            //}
+            #endregion
+
+
+
+            Console.ReadKey(); 
+        }
+    }
+}
