726284dce66a2f94657b385a71386c3767bc5cfb
CaioProg <caioprog18@gmail.com>
2022-06-20 15:51:11 -0300

    Sobrecarga de construtor (Overloading)

--- POO/MetodoConstrutor/Program.cs
+++ POO/MetodoConstrutor/Program.cs
@@ -12,12 +12,28 @@ namespace MetodoConstrutor
         {
             Pessoa p1 = new Pessoa();
 
-            p1.nome = "Caio";
-
             Console.WriteLine(p1.nome);
             Console.WriteLine(p1.sobrenome);
             Console.WriteLine(p1.anoNascimento);
             Console.WriteLine(p1.idade);
+
+            Console.WriteLine();
+
+            Pessoa p2 = new Pessoa("Caio", "Lucas", 2002);
+
+            Console.WriteLine(p2.nome);
+            Console.WriteLine(p2.sobrenome);
+            Console.WriteLine(p2.anoNascimento);
+            Console.WriteLine(p2.idade);
+
+            Console.WriteLine();
+
+            Pessoa p3 = new Pessoa("Caio", "Lucas");
+
+            Console.WriteLine(p3.nome);
+            Console.WriteLine(p3.sobrenome);
+            Console.WriteLine(p3.anoNascimento);
+            Console.WriteLine(p3.idade);
 
             Console.ReadKey();
         }
