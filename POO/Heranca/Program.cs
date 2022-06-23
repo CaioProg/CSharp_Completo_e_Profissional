9c21da2960c3a16c161f390cdc38eedf91e50a8c
CaioProg <caioprog18@gmail.com>
2022-06-23 17:46:31 -0300

    Membros saeled

--- POO/Heranca/Program.cs
+++ POO/Heranca/Program.cs
@@ -10,15 +10,33 @@ namespace Heranca
     {
         static void Main(string[] args)
         {
-            Carro c = new Carro();
-            Bicicleta b = new Bicicleta();
+            #region Parte 1
+            //Carro c = new Carro();
+            //Bicicleta b = new Bicicleta();
 
 
-            c.Acelerar();
-            b.Acelerar();
+            //c.Acelerar();
+            //b.Acelerar();
 
-            c.Parar();
-            b.Parar();
+            //c.Parar();
+            //b.Parar();
+            #endregion
+
+            Humano a = new Humano();
+            Pessoa b = new Pessoa();
+            Homem c = new Homem();
+
+            Console.WriteLine("\nHumano");
+            a.Olhos();
+            a.Cabelos();
+
+            Console.WriteLine("\nPessoa");
+            b.Olhos();
+            b.Cabelos();
+
+            Console.WriteLine("\nHomem");
+            c.Olhos();
+            c.Cabelos();
 
             Console.ReadKey();
         }
