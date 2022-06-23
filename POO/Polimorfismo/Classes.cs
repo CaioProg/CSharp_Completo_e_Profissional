7395bea85be8e2112068d77232d324fb5b5f0e74
CaioProg <caioprog18@gmail.com>
2022-06-23 18:27:25 -0300

    Polimorfismo

---
+++ POO/Polimorfismo/Classes.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    abstract public class Forma
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Altura { get; set; }
+        public int Largura { get; set; }
+        public int Raio { get; set; }
+
+        public virtual void Desenhar()
+        {
+            Console.WriteLine("Preparando-se para desenhar");
+        }
+        public virtual void Area()
+        {
+            double area = Largura * Altura;
+            Console.WriteLine($"Area: {area}");
+
+        }
+    }
+
+    public class Circulo : Forma
+    {
+        public override void Desenhar()
+        {
+            base.Desenhar();
+            Console.WriteLine("Desenhando um Circulo");
+        }
+        public override void Area()
+        {
+            double area = 3.14 * (Raio * Raio);
+            Console.WriteLine($"Area Circulo {area}");
+        }
+    }
+
+    public class Retangulo : Forma
+    {
+        public override void Desenhar()
+        {
+            // Code to draw a Retangulo
+            base.Desenhar();
+            Console.WriteLine("Desenhando um Retangulo");
+        }
+        public override void Area()
+        {
+            base.Area();
+        }
+    }
+
+    public class Triangulo : Forma
+    {
+        public override void Desenhar()
+        {
+            // Code to draw a Triangulo
+            base.Desenhar();
+            Console.WriteLine("Desenhando um Triangulo");
+            base.Desenhar();
+        }
+        public override void Area()
+        {
+            double area = (Largura * Altura) / 2;
+            Console.WriteLine($"Area Triangulo {area}");
+        }
+    }
+}
