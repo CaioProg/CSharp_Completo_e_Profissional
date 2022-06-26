34108d238dc026f921a627ed1e1a5b74c3793a78
CaioProg <caioprog18@gmail.com>
2022-06-26 19:12:58 -0300

    Contains - ToString

---
+++ ManipularString/ManipularString/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManipularString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            //string texto = "Este tipo de variavel é alfanumerica.";
+
+            //if (texto.Contains("tipo de v"))
+            //{
+            //    label1.Text = "Contém.";
+            //}
+            //else
+            //{
+            //    label1.Text = "Não Contém.";
+            //}
+
+            int num = 5400;
+            bool res = true;
+
+            label1.Text = res.ToString();
+        }
+    }
+}
