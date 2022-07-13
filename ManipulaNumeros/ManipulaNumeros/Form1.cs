8db320a0100f7679d1c1575be8a1152071b5733b
CaioProg <caioprog18@gmail.com>
2022-07-13 19:49:54 -0300

    Método TryParse

---
+++ ManipulaNumeros/ManipulaNumeros/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManipulaNumeros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num;
+            //bool res = int.TryParse("2002aa",out num);
+
+            if (int.TryParse("2002", out num))
+            {
+                lblResultado.Text = num.ToString();
+            }
+            else
+            {
+                lblResultado.Text = "Erro na conversão";
+            }
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
