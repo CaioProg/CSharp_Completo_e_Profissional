3df941cb8c7f2e2c71584591a63289a6448f4f56
CaioProg <caioprog18@gmail.com>
2022-07-13 20:09:30 -0300

    Método ToString

--- ManipulaNumeros/ManipulaNumeros/Form1.cs
+++ ManipulaNumeros/ManipulaNumeros/Form1.cs
@@ -19,17 +19,34 @@ namespace ManipulaNumeros
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int num;
+            #region TryParse
+            //int num;
             //bool res = int.TryParse("2002aa",out num);
 
-            if (int.TryParse("2002", out num))
-            {
-                lblResultado.Text = num.ToString();
-            }
-            else
-            {
-                lblResultado.Text = "Erro na conversão";
-            }
+            //if (int.TryParse("2002", out num))
+            //{
+            //    lblResultado.Text = num.ToString();
+            //}
+            //else
+            //{
+            //    lblResultado.Text = "Erro na conversão";
+            //}
+            #endregion
+
+            #region ToString / Convert.ToString
+            //double num = 120.5524;
+            //lblResultado.Text = num.ToString("#.00");
+
+            //int num = 254;
+            //lblResultado.Text = num.ToString("X"); // Hex
+            //lblResultado.Text = num.ToString("D"); // Dec
+            //lblResultado.Text = num.ToString("X2"); // Hex min 2 digitos
+            //lblResultado.Text = num.ToString("D2"); // Dec min 2 digitos
+            //lblResultado.Text = num.ToString("C"); // Monetario
+
+            //lblResultado.Text = Convert.ToString(num, 8);
+            #endregion
+
         }
 
         private void lblResultado_Click(object sender, EventArgs e)
