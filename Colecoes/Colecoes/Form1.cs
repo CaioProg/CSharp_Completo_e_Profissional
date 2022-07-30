7643eb19b80e2e9577a8a2d53655057afb0e0a64
CaioProg <caioprog18@gmail.com>
2022-07-30 18:18:20 -0300

    List - Parte 2

--- Colecoes/Colecoes/Form1.cs
+++ Colecoes/Colecoes/Form1.cs
@@ -19,11 +19,12 @@ namespace Colecoes
 
         private void btnList_Click(object sender, EventArgs e)
         {
+            lista.Items.Clear();
             // Array 
             string[] nomes = new string[3];
             nomes[0] = "Caio";
             nomes[1] = "Julia";
-            nomes[2] = "Lucas";
+            nomes[2] = "Joca";
 
             // Lista Generica
             List<string> nomes2 = new List<string>();
@@ -31,6 +32,38 @@ namespace Colecoes
             nomes2.Add("Lucas");
             nomes2.Add("Beatriz");
             nomes2.AddRange(nomes);
+
+            //if (nomes2.Remove("Lucas"))
+            //{
+            //    MessageBox.Show("Lucas removido");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Não foi possivel remover.");
+            //}
+
+            //if (nomes2.Contains("Lucas"))
+            //{
+            //    MessageBox.Show("Contém");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Não Contém");
+            //}
+
+            //MessageBox.Show("Número de elementos: " + nomes2.Count);
+
+            //nomes2.Sort();
+
+            //MessageBox.Show("Julia está no índice: " + nomes2.IndexOf("Julia"));
+
+            //nomes2.Insert(2, "João");
+
+            //nomes2.RemoveAt(1);
+
+            //nomes2.Clear();
+
+            nomes2.Add("Lucas");
 
             foreach (string nome in nomes2)
             {
